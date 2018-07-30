bc0978d80a216390516af0a8c12747af8031ee8a
drshivago <moessner2015@gmail.com>
2018-07-30 08:27:32 -0400

    removed try statement

--- camera.py
+++ camera.py
@@ -4,7 +4,7 @@ import time
 framesize = (640, 480)
 camera = PiCamera()
 
-try: 
-	for filename in camera.capture_continuous('img{counter:03d}.jpg):
-		print('Captured %s' % filename)
-		sleep(30) #wait 30 seconds
+
+for filename in camera.capture_continuous(f'img{counter:03d}.jpg):
+	print('Captured %s' % filename)
+	sleep(30) #wait 30 seconds
